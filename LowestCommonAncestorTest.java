18c92b9ec0220279e348f4b303d24c1a5ad0e800
nialllee <55655439+nialllee@users.noreply.github.com>
2019-10-07 12:38:57 +0100

    Added test for not present nodes

--- LowestCommonAncestorTest.java
+++ LowestCommonAncestorTest.java
@@ -31,4 +31,22 @@ public class LowestCommonAncestorTest{
     
   }
   
+  @Test
+  public void testNodesNotPresent(){
+    
+    LowestCommonAncestor tree = new LowestCommonAncestor();
+    tree.root = new Node (5);
+    tree.root.left = new Node(6);
+    tree.root.right = new Node(7);
+    tree.root.left.left = new Node(8);
+		tree.root.left.right = new Node(9);
+		tree.root.right.left = new Node(10);
+		tree.root.right.right = new Node(11);
+  
+    assertEquals("Finds ancestors of nodes not present in populated tree: ", null, tree.findLowestCommonAncestor(15, 16));
+		assertEquals("Finds ancestors of nodes not present in populated tree: ", null, tree.findLowestCommonAncestor(52, 87));
+  }
+  
+}  
+  
   
