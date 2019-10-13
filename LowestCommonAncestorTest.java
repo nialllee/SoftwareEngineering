f5065be8a3709ed9a3b145314ded53480c64d539
nialllee <55655439+nialllee@users.noreply.github.com>
2019-10-13 12:05:16 +0100

    test to check common ancestor of present nodes

--- LowestCommonAncestorTest.java
+++ LowestCommonAncestorTest.java
@@ -39,14 +39,33 @@ public class LowestCommonAncestorTest{
     tree.root.left = new Node(6);
     tree.root.right = new Node(7);
     tree.root.left.left = new Node(8);
-		tree.root.left.right = new Node(9);
-		tree.root.right.left = new Node(10);
-		tree.root.right.right = new Node(11);
+    tree.root.left.right = new Node(9);
+    tree.root.right.left = new Node(10);
+    tree.root.right.right = new Node(11);
   
     assertEquals("Finds ancestors of nodes not present in populated tree: ", null, tree.findLowestCommonAncestor(15, 16));
-		assertEquals("Finds ancestors of nodes not present in populated tree: ", null, tree.findLowestCommonAncestor(52, 87));
+    assertEquals("Finds ancestors of nodes not present in populated tree: ", null, tree.findLowestCommonAncestor(52, 87));
   }
   
+  @Test
+  public void testNodesPresent(){
+	  
+    LowestCommonAncestor tree = new LowestCommonAncestor();
+    tree.root = new Node (5);
+    tree.root.left = new Node(6);
+    tree.root.right = new Node(7);
+    tree.root.left.left = new Node(8);
+    tree.root.left.right = new Node(9);
+    tree.root.right.left = new Node(10);
+    tree.root.right.right = new Node(11);
+	  
+    assertEquals("LCA of 8 and 9: ", 6, tree.findLCA(8, 9).data);
+    assertEquals("LCA of 10 and 11: ", 7, tree.findLCA(10, 11).data);
+    assertEquals("LCA of 6 and 7: ", 5, tree.findLCA(6, 7).data);	
+    assertEquals("LCA of 8 and 11: ", 5, tree.findLCA(8, 11).data);	  
+	  
 }  
   
-  
+  		//		5
+		//	6		7
+		//    8   9   	     10   11
