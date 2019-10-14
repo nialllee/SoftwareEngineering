1f1b52a58a1b8d2798c41b8dba9864366d1c847a
nialllee <55655439+nialllee@users.noreply.github.com>
2019-10-14 11:49:20 +0100

    made a DAG_Node file

---
+++ DAG_Node.java
@@ -0,0 +1,20 @@
+import java.util.*;
+
+
+public class Node_DAG<T> {
+	
+  Set<Node_DAG<T>> point;
+	T data;
+	
+	public Node_DAG(T data){
+		
+		this.data = data;
+		this.point = new HashSet<>();
+	}
+	
+	public void addEdge(Node_DAG<T> edge){
+		
+		point.add(edge);
+	}
+	
+}
