26e21d9d48e8d4af113d9c1ab5ead936e6174836
nialllee <55655439+nialllee@users.noreply.github.com>
2019-10-15 12:50:22 +0100

    Think Class Is Finished

--- LowestCommonAncestor.java
+++ LowestCommonAncestor.java
@@ -23,3 +23,46 @@ public class LowestCommonAncestor<T> {
 
 		  return LowestCommonAncestor_DAG(root, node1, node2);
 	  }
+          public Node_DAG<T> LCA_DAG(Node_DAG<T> root, Node_DAG<T> node1, Node_DAG<T> node2){
+		  
+		  List<Node_DAG<T>> list1 = DFS(root, node1, new ArrayList<>(), new Stack<>());
+		  List<Node_DAG<T>> list2 = DFS(root, node2, new ArrayList<>(), new Stack<>());
+		  List<Node_DAG<T>> minimumList;
+		  List<Node_DAG<T>> maximumList;
+		  
+		  if(list1.size() <= list2.size()){
+			  minimumList = list1;
+			  maximumList = list2;
+		  }
+		  else{
+			  minimumList = list2;
+			  maximumList = list1;
+		  }
+		  
+		  if(root == null || node1 == null || node2 == null){
+			  
+			  return null;
+		  }
+		  
+		  if(list1 == null || list2 == null){
+			  
+			  return null;
+		  }
+		  
+		  Set<Node_DAG<T>> set = new HashSet<>();
+		  
+		  for(Node_DAG<T> n : minimumList){
+			  set.add(n);
+		  }
+		  
+		  for(int j = maximumList.size(); j >= 0; j--){
+			  
+			  if(set.contains(maximumList.get(j))){
+				  
+				  return maximumList.get(j);
+			  }
+		  }
+		  
+		  return null;  
+	  }
+}
