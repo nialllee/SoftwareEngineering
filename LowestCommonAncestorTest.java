d0097ccfd51fd73bd14d2873cff1d0cc7ee34905
nialllee <55655439+nialllee@users.noreply.github.com>
2019-10-16 16:32:11 +0100

    fixed an accidental push

--- LowestCommonAncestorTest.java
+++ LowestCommonAncestorTest.java
@@ -1,62 +1,70 @@
 import static org.junit.Assert.*;
 import org.junit.Test;
 
-public class DAGTest {
+public class lowestCommonAncestorTest {
 	
-	@Test
-	public void testDAG()
-	{
-		DAG graph = new DAG(10);
-		
-		graph.addEdge(1, 2);
-		graph.addEdge(2, 4);
-		graph.addEdge(2, 5);
-		graph.addEdge(4, 6);
-		graph.addEdge(4, 7);
-		
-		assertEquals(1, graph.indegree(4));
-		assertEquals(2, graph.outdegree(4));
-		assertEquals(5, graph.E());
-		assertEquals(10, graph.V());
-		String adj = "[6, 7]";
-		assertEquals(adj, graph.adj(4).toString());
-	}
-	
-	@Test(expected=Exception.class)
-	public void exceptionTest(){
-		
-		//Directed graph needs to have positive number of vertices
-		
-		DAG graph = new DAG(-5);
-	}
-	
-	@Test
-	public void addEdge()
-	{
-		DAG graph = new DAG(5);
-		
-		graph.addEdge(1,2);
+ @Test
+  public void testEmptyTree(){
 
-		//Negative would not addEdge
-		graph.addEdge(-1, -6);
-		
-		//12 is larger than 5 therefore edge won't be added
-		graph.addEdge(3, 12);
-		
-		assertEquals(1, graph.E());
-	}
-	
-	@Test
-	public void testOutdegree()
-	{
-		DAG graph = new DAG(5);
-		
-		graph.addEdge(1, 2);
-		graph.addEdge(2, 4);
-		graph.addEdge(3, 3);
-		
-		assertEquals(1, graph.outdegree(3));
-	
-		assertEquals(-1, graph.outdegree(5));
-	}
-	
+    LowestCommonAncestor tree = new LowestCommonAncestor();
+    assertNull("Finds the Lowest Common Ancestor when the tree is empty ", tree.findLowestCommonAncestor(1, 2));
+    assertEquals("Finds the Lowest Common Ancestor when the tree is empty ", null, tree.findLowestCommonAncestor(1, 2) );
+
+  }
+
+  @Test
+  public void testNodeConstructor(){
+
+    Node temp = new Node(1);
+    assertNotNull("Tests the Node Constructor ", temp);
+
+  }  
+
+  @Test
+  public void test2Nodes(){
+
+    LowestCommonAncestor tree = new LowestCommonAncestor();
+    tree.root = new Node(1);
+    tree.root.left = new Node(2);
+    assertEquals("Finds the Lowest Common Ancestor of a one root & one child tree ", 1, tree.findLowestCommonAncestor(1, 2).data);
+
+  }
+
+  @Test
+  public void testNodesNotPresent(){
+
+    LowestCommonAncestor tree = new LowestCommonAncestor();
+    tree.root = new Node (5);
+    tree.root.left = new Node(6);
+    tree.root.right = new Node(7);
+    tree.root.left.left = new Node(8);
+    tree.root.left.right = new Node(9);
+    tree.root.right.left = new Node(10);
+    tree.root.right.right = new Node(11);
+
+    assertEquals("Finds ancestors of nodes not present in populated tree: ", null, tree.findLowestCommonAncestor(15, 16));
+    assertEquals("Finds ancestors of nodes not present in populated tree: ", null, tree.findLowestCommonAncestor(52, 87));
+  }
+
+  @Test
+  public void testNodesPresent(){
+
+    LowestCommonAncestor tree = new LowestCommonAncestor();
+    tree.root = new Node (5);
+    tree.root.left = new Node(6);
+    tree.root.right = new Node(7);
+    tree.root.left.left = new Node(8);
+    tree.root.left.right = new Node(9);
+    tree.root.right.left = new Node(10);
+    tree.root.right.right = new Node(11);
+
+    assertEquals("LCA of 8 and 9: ", 6, tree.findLowestCommonAncestor(8, 9).data);
+    assertEquals("LCA of 10 and 11: ", 7, tree.findLowestCommonAncestor(10, 11).data);
+    assertEquals("LCA of 6 and 7: ", 5, tree.findLowestCommonAncestor(6, 7).data);	
+    assertEquals("LCA of 8 and 11: ", 5, tree.findLowestCommonAncestor(8, 11).data);	  
+
+}  
+
+  		//		5
+		//	6		7
+		//    8   9   	     10   11
