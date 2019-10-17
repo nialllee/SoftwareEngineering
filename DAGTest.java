c450e8cafb4d8225ee1754c80914579cdefbdc1c
nialllee <55655439+nialllee@users.noreply.github.com>
2019-10-17 13:56:03 +0100

    Think final tests added

--- DAGTest.java
+++ DAGTest.java
@@ -63,7 +63,6 @@ public class DAG_Test {
 		graph.addEdge(3, 3);
 		
 		assertEquals(1, graph.outdegree(3));
-	
 		assertEquals(-1, graph.outdegree(5));
 	}
 	
@@ -169,5 +168,39 @@ public class DAG_Test {
 		assertEquals(-1, graph.findLowestCommonAncestor(1, 3));
 		assertEquals(-1, graph.findLowestCommonAncestor(0, 3));
 		assertEquals(-1, graph.findLowestCommonAncestor(1, 2));
-		
 	}
+	
+	@Test
+	public void testLCACommonAncestors()
+	{
+		DAG graph = new DAG(10);
+		graph.addEdge(0, 1);
+		graph.addEdge(0, 3);
+		graph.addEdge(1, 3);
+		graph.addEdge(3, 2);
+		graph.addEdge(2, 4);
+		graph.addEdge(1, 5);
+		graph.addEdge(2, 5);
+		//test LCA
+		assertEquals(0, graph.findLowestCommonAncestor(2, 1));
+		assertEquals(3, graph.findLowestCommonAncestor(2, 3));
+		assertEquals(2, graph.findLowestCommonAncestor(4, 5));
+		//Test vertex that aren't there
+		assertEquals(-1, graph.findLowestCommonAncestor(8, 2));
+		//tests -ve vertex
+		assertEquals(-1, graph.findLowestCommonAncestor(-2, 3));
+		assertEquals(-1, graph.findLowestCommonAncestor(3, -2));
+		assertEquals(-1, graph.findLowestCommonAncestor(-2, -3));
+	}
+	
+	@Test
+	public void testLCASameVertex()
+	{
+		DAG graph = new DAG(10);
+		graph.addEdge(0, 1);
+		graph.addEdge(0, 3);
+		graph.addEdge(1, 3);
+		
+		assertEquals(3, graph.findLowestCommonAncestor(3, 3));
+	}
+}
