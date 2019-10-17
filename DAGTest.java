4521450917cd5ce52f8b78c2fad5966361dad24c
nialllee <55655439+nialllee@users.noreply.github.com>
2019-10-17 12:00:55 +0100

    Update DAGTest.java

--- DAGTest.java
+++ DAGTest.java
@@ -100,7 +100,7 @@ public class DAG_Test {
 	@Test
 	public void testCycle()
 	{
-		DAG graph = new DAG(10);
+		DAG graph = new DAG(5);
 		graph.addEdge(0, 1);
 		graph.addEdge(1, 2);
 		graph.addEdge(2, 0);
@@ -114,11 +114,33 @@ public class DAG_Test {
 	@Test
 	public void testAcyclicGraph()
 	{
-		DAG graph = new DAG(10);
+		DAG graph = new DAG(5);
 		graph.addEdge(1, 2);
 		graph.addEdge(2, 4);
 		graph.addEdge(3, 3);
 		
 		graph.findCycle(1);
 		assertFalse(graph.hasCycle());
+	}
+	
+		@Test
+	public void testLCA()
+	{
+		DAG graph = new DAG(10);
+		graph.addEdge(0, 1);
+		graph.addEdge(1, 3);
+		graph.addEdge(1, 2);
+		graph.addEdge(3, 4);
+		graph.addEdge(2, 5);
+		graph.addEdge(5, 6);
+		graph.addEdge(4, 6);
+		graph.addEdge(6, 7);
+		graph.addEdge(4, 8);
+		graph.addEdge(8, 7);
+		
+		assertEquals(0, graph.findLowestCommonAncestor(0, 1));
+		assertEquals(1, graph.findLowestCommonAncestor(2, 3));
+		assertEquals(1, graph.findLowestCommonAncestor(4, 5));
+		assertEquals(8, graph.findLowestCommonAncestor(7, 8));
+		assertEquals(6, graph.findLowestCommonAncestor(6, 7));
 	}
